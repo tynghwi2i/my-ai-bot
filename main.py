768eb14c8debd115e9434ee16a0d424d280d41ce
tynghwi2i <tynchtykmamiev@gmail.com>
2026-04-07 17:46:09 +0600

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ BOT_TOKEN = os.environ.get('BOT_TOKEN')
 AI_KEY = os.environ.get('AI_KEY')
 
 genai.configure(api_key=AI_KEY)
-model = genai.GenerativeModel('gemini-1.5-flash-latest')
+model = genai.GenerativeModel('gemini-1.5-flash')
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=['start'])
